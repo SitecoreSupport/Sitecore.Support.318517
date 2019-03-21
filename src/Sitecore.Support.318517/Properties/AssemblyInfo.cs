183b805bad5a9dac1d42f02fed5182f47e562a4a
Paul Stupka <paul.stupka@sitecore.com>
2019-03-21 17:36:59 +0200

    init

---
+++ src/Sitecore.Support.318517/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.318517")]
+[assembly: AssemblyProduct("Sitecore.Support.318517")]
+[assembly: ComVisible(false)]
