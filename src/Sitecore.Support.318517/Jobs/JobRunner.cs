91892cf50b0b96a5c471982ffbc81e8e7ed76e37
Paul Stupka <paul.stupka@sitecore.com>
2019-03-21 17:46:37 +0200

    fix the issue

---
+++ src/Sitecore.Support.318517/Jobs/JobRunner.cs
@@ -0,0 +1,19 @@
+﻿namespace Sitecore.Support.Jobs
+{
+  using Sitecore.Jobs;
+  using Sitecore.Security.Accounts;
+  public class JobRunner
+  {
+    /// <summary>
+    /// Restores the original context user
+    /// </summary>
+    /// <param name="args">The arguments.</param>
+    public static void RestoreContextUser(JobArgs args)
+    {
+      if (args.Job.Options.ContextUser != null)
+      {
+        UserSwitcher.Exit();
+      }
+    }
+  }
+}
--- src/Sitecore.Support.318517/Properties/AssemblyInfo.cs
+++ src/Sitecore.Support.318517/Properties/AssemblyInfo.cs
@@ -4,3 +4,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("Sitecore.Support.318517")]
 [assembly: AssemblyProduct("Sitecore.Support.318517")]
 [assembly: ComVisible(false)]
+[assembly: AssemblyDescription("Memory leak when running jobs in context of an explicitly set user")]
+
